7f46e86856b3c70eb238417fef7e93e6b1342b16
mariusbaumann <baumann.marius@gmail.com>
2015-10-16 10:52:31 +0200

    first setup

---
+++ jsmanager_virtenv/jsmanager/lagerplaner/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
+def index(request):
+    template = loader.get_template('jsmanager/index.html')
+    context = RequestContext(request, {
+            
+        })
+    return HttpResponse(template.render(context))
