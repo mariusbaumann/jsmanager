af96960996390e5579761fb4180303252c2e5a20
mariusbaumann <baumann.marius@gmail.com>
2015-10-25 19:31:20 +0100

    collectstatics

--- jsmanager_virtenv/jsmanager/jsmanager/settings.py
+++ jsmanager_virtenv/jsmanager/jsmanager/settings.py
@@ -101,7 +101,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/Users/baumanma/GIT/jsmanager/jsmanager_virtenv/jsmanager/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/template'),
+    os.path.join(BASE_DIR, 'static/jscss'),
 )
